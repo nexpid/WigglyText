08bdc32be0c6deae829346de478b25a1986fcdfd
ScattrdBlade <61068313+ScattrdBlade@users.noreply.github.com>
2025-08-22 16:30:21 -0600

    Implement Vencord Changes
    Fix the imports to use new variables Vencord now uses.

--- index.tsx
+++ index.tsx
@@ -5,8 +5,7 @@
  */
 
 import { definePluginSettings } from "@api/Settings";
-import { makeRange } from "@components/PluginSettings/components";
-import definePlugin, { OptionType } from "@utils/types";
+import definePlugin, { makeRange, OptionType } from "@utils/types";
 import { Text } from "@webpack/common";
 import { ReactNode } from "react";
 
